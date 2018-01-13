3559ebaf0857e5bcc2c0697991cd86dec8d5bac6
miholeus <me@miholeus.com>
2018-01-13 20:07:15 +0300

    removed unused code :art:

--- neo4j_search.py
+++ neo4j_search.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 
 from py2neo import Graph
-import re
 
 graph_db = Graph("http://neo4j:7474/db/data")
 
@@ -13,8 +12,6 @@ return p, length(p) as L
 order by L desc
 limit 1
 """
-
-p = re.compile('\(\w+\)')
 
 data = graph_db.data(query)
 
